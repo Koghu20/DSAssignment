d2e1b8b8d427b638b76744125e915def33d4e00b
Koghu <Koghu@ThaNorTy-Laptop>
2019-09-27 03:34:59 +0800

    wic190018

---
+++ WIC190018/src/Week01/L01Q02.java
@@ -0,0 +1,22 @@
+
+package Week01;
+
+public class L01Q02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("*********"+"\t   ***"+"\t     *"+"\t     *");
+        System.out.println("*       *"+"\t  *    *"+"\t    ***"+"\t    * *");
+        System.out.println("*       *"+"\t *      *"+"\t*****"+"\t   *   *");
+        System.out.println("*       *"+"\t *      *"+"\t  *"+"\t  *     *");
+        System.out.println("*       *"+"\t *      *"+"\t  *"+"\t *       *");
+        System.out.println("*       *"+"\t *      *"+"\t  *"+"\t  *     *");
+        System.out.println("*       *"+"\t *      *"+"\t  *"+"\t   *   *");
+        System.out.println("*       *"+"\t  *    *"+"\t     *"+"\t    * *");
+        System.out.println("*********"+"\t   ***"+"\t     *"+"\t     *");
+        // TODO code applicasout    tion logic here
+    }
+    
+}
